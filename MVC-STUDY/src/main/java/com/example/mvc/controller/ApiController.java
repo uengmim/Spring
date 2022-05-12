ef4e05241426cfb9586fb14a835461de4dfef25e
uengmim <72143238+uengmim@users.noreply.github.com>
2022-05-12 18:50:03 +0900

    Add files via upload

---
+++ MVC-STUDY/src/main/java/com/example/mvc/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.mvc.controller;
+
+
+import com.example.mvc.dto.UserReq;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    public UserReq user(UserReq userReq){
+        return userReq;
+    }
+}

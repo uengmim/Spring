c8726eca5490735bd284a36c2e917b772cff9cc0
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-05-31 10:42:48 +0900

    Swagger

--- swagger/src/main/java/com/example/swagger/controller/ApiController.java
+++ swagger/src/main/java/com/example/swagger/controller/ApiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"API 정보를 제공하는 Controller"})
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {

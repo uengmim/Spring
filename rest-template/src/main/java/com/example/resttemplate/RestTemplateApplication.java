bae7e698513627943e02506cfa63e71ce80463be
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-09 20:06:33 +0900

    RestTemplateApplication

--- rest-template/src/main/java/com/example/resttemplate/RestTemplateApplication.java
+++ rest-template/src/main/java/com/example/resttemplate/RestTemplateApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestTemplateApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(RestTemplateApplication.class, args);
     }
 

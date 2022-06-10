ab5dd4d50f5c6f13172af7319e0de6debc3c0f97
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-10 20:55:01 +0900

    java

--- filter-and-interceptor/src/main/java/com/example/filterandinterceptor/FilterAndInterceptorApplication.java
+++ filter-and-interceptor/src/main/java/com/example/filterandinterceptor/FilterAndInterceptorApplication.java
@@ -2,12 +2,14 @@ package com.example.filterandinterceptor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class FilterAndInterceptorApplication {
+@ServletComponentScan
+public class filterandinterceptorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FilterAndInterceptorApplication.class, args);
+        SpringApplication.run(filterandinterceptorApplication.class, args);
     }
 
 }

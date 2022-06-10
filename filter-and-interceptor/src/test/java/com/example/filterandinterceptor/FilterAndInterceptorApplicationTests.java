86d078635624268164d1a3cf90333748d7e9793d
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-10 20:58:12 +0900

    test

--- filter-and-interceptor/src/test/java/com/example/filterandinterceptor/FilterAndInterceptorApplicationTests.java
+++ filter-and-interceptor/src/test/java/com/example/filterandinterceptor/FilterAndInterceptorApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.filterandinterceptor;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootTest
-class FilterAndInterceptorApplicationTests {
+@SpringBootApplication
+@ServletComponentScan
+public class FilterandinterceptorApplication {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(FilterandinterceptorApplication.class, args);
     }
 
 }

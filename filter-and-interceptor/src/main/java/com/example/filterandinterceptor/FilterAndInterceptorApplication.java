192efad280c2512783ef0520edfbf503e256fa36
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-10 20:49:39 +0900

    filter-interceptor

---
+++ filter-and-interceptor/src/main/java/com/example/filterandinterceptor/FilterAndInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.example.filterandinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterAndInterceptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterAndInterceptorApplication.class, args);
+    }
+
+}

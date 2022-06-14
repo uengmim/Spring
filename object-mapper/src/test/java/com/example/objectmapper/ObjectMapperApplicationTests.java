bbcfe6ff6d105f4773098bdf71fe44e8749c8eb9
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-14 18:08:55 +0900

    test

--- object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
+++ object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.objectmapper;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+package dto;
 
-@SpringBootTest
-class ObjectMapperApplicationTests {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    @Test
-    void contextLoads() {
+public class Car {
+    private String name;
+
+    @JsonProperty("car_number")
+    private String carNumber;
+
+    @JsonProperty("TYPE")
+    private String type;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }

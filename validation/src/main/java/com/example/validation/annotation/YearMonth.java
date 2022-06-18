49c526d4b89c139db01c1ceaf88ceeb045b1df1f
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-19 03:54:37 +0900

    java

--- validation/src/main/java/com/example/validation/annotation/YearMonth.java
+++ validation/src/main/java/com/example/validation/annotation/YearMonth.java
@@ -24,3 +24,19 @@ public @interface YearMonth {
 
     String pattern() default "yyyyMMdd";
 }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

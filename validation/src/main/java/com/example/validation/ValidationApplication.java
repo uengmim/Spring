c88617aba194a44e254f040488ea375b6d742b9e
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-18 23:01:41 +0900

    test

--- validation/src/main/java/com/example/validation/ValidationApplication.java
+++ validation/src/main/java/com/example/validation/ValidationApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ValidationApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(ValidationApplication.class, args);
     }

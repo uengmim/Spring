cd392a084a03b2260f8255c14638b4a84b985b21
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-20 19:37:24 +0900

    java

--- validation/src/test/java/com/example/validation/ValidationApplicationTests.java
+++ validation/src/test/java/com/example/validation/ValidationApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example.validation.annotation;
 
+import com.example.validation.dto.Car;
 import com.example.validation.validator.YearMonthValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.util.List;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.ElementType.TYPE_USE;
@@ -14,13 +16,47 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = {YearMonthValidator.class})
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
-public @interface YearMonth {
+public class User {
 
-    String message() default "yyyyMM 형식에 맞지 않습니다.";
+    @NotBlank
+    private String name;
 
-    Class<?>[] groups() default { };
+    @Max(value = 90)
+    private int age;
 
-    Class<? extends Payload>[] payload() default { };
+    @Valid
+    private List<Car> cars;
 
-    String pattern() default "yyyyMMdd";
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", cars=" + cars +
+                '}';
+    }
 }

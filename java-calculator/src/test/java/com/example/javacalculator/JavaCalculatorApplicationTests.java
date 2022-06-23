a50f582baeebfa3d75b741c9f1ee61c3c84726b7
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-23 16:51:18 +0900

    java-calculator

---
+++ java-calculator/src/test/java/com/example/javacalculator/JavaCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

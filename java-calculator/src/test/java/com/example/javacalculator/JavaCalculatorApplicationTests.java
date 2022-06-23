445a034566f56bb2c9d9895a006b39578acc7273
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-23 16:53:34 +0900

    Test

--- java-calculator/src/test/java/com/example/javacalculator/JavaCalculatorApplicationTests.java
+++ java-calculator/src/test/java/com/example/javacalculator/JavaCalculatorApplicationTests.java
@@ -1,13 +1,40 @@
-package com.example.javacalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+@ExtendWith(MockitoExtension.class)
+public class Test {
+    @Mock
+    public MarketServer marketServer;
 
-@SpringBootTest
-class JavaCalculatorApplicationTests {
-
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    public void init(){
+        Mockito.lenient().when(marketServer.price()).thenReturn(200);
     }
 
+    @org.junit.jupiter.api.Test
+    public void hello(){
+        System.out.println("hello");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void dollar(){
+        DollarCalculator dollarCalculator = new DollarCalculator(null);
+        Calculator calculator = new Calculator(dollarCalculator);
+        int sum = calculator.sum(100, 100);
+        Assertions.assertEquals(20000, sum);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void mock(){
+        DollarCalculator dollarCalculator = new DollarCalculator(marketServer);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+        int sum = calculator.sum(100, 100);
+        Assertions.assertEquals(40000, sum);
+    }
 }

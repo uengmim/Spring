7c6b3190ed898a6cde476c40e2b81fc82298dd4d
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-01 14:59:16 +0900

    properties

---
+++ springboot/src/main/java/com/example/springboot/SpringbootProperties.java
@@ -0,0 +1,27 @@
+package me.springboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("holoman")
+public class SpringbootProperties {
+
+    private String name;
+
+    private int howLong;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHowLong() {
+        return howLong;
+    }
+
+    public void setHowLong(int howLong) {
+        this.howLong = howLong;
+    }
+}

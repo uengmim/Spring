754f46e50324b060e026ebc49345027a83bfb3b8
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-01 15:20:02 +0900

    app runner

---
+++ spring-aop/src/main/java/com/example/springaop/AppRunner.java
@@ -0,0 +1,20 @@
+package com.rldnddl87.springaop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+
+    @Autowired
+    EventService eventService;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        eventService.createEvent();
+        eventService.publishEvent();
+        eventService.deleteEvent();
+    }
+}

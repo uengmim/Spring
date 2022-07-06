3eae5a0476fcb37861b7507347c283762eefe413
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-06 14:28:33 +0900

    entity_order

---
+++ springboot-admin/src/main/java/com/example/springbootadmin/model/entity/OrderDetail.java
@@ -0,0 +1,59 @@
+package com.admin.springbootadmin.model.entity;
+
+import com.admin.springbootadmin.model.enumClass.OrderStatus;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = {"orderGroup","item"})
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Accessors(chain = true)
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private LocalDateTime arrivalDate;
+
+    private Integer quantity;
+
+    private BigDecimal totalPrice;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    // OrderDetail N : 1 OrderGroup
+    @ManyToOne
+    private OrderGroup orderGroup;
+
+    // OrderDetail N : 1 Item
+    @ManyToOne
+    private Item item;
+
+}
---
+++ springboot-admin/src/main/java/com/example/springbootadmin/model/entity/OrderGroup.java
@@ -0,0 +1,75 @@
+package com.admin.springbootadmin.model.entity;
+
+import com.admin.springbootadmin.model.enumClass.OrderStatus;
+import com.admin.springbootadmin.model.enumClass.OrderType;
+import com.admin.springbootadmin.model.enumClass.PaymentType;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@ToString(exclude = {"user","orderDetailList"})
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Accessors(chain = true)
+public class OrderGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType; //주문의 형태 일괄/개별
+
+    private String revAddress;
+
+    private String revName;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentType paymentType;
+
+    private BigDecimal totalPrice;
+
+    private Integer totalQuantity;
+
+    private LocalDateTime orderAt;
+
+    private LocalDateTime arrivalDate;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    // OrderGroup N : 1 User
+    @ManyToOne
+    private User user;
+
+    // OrderGroup 1 : N OrderDetail
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "orderGroup")
+    private List<OrderDetail> orderDetailList;
+
+
+}

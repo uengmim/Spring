64dbd6eb23792c4de032e10503dbe7b3ffe595dc
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-06 14:32:21 +0900

    header

---
+++ springboot-admin/src/main/java/com/example/springbootadmin/model/network/Header.java
@@ -0,0 +1,69 @@
+package com.admin.springbootadmin.model.network;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Header<T> {
+
+
+    //api 통신 시간
+    private LocalDateTime transactionTime;
+
+    //api 응답 코드
+    private String resultCode;
+
+    //api 설명
+    private String description;
+
+    private T data;
+
+    private Pagination pagination;
+
+
+    //data가 없고 응답이 정상적인 경우
+    public static <T> Header<T> OK() {
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .build();
+    }
+
+    //data가 있고 응답이 정상적인 경우
+    public static <T> Header<T> OK(T data) {
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .data(data)
+                .description("OK")
+                .build();
+    }
+
+    //error
+    public static <T> Header<T> ERROR(String description) {
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("ERROR")
+                .description(description)
+                .build();
+    }
+
+    public static <T> Header<T> OK(T data, Pagination pagination){
+        return (Header<T>)Header.builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .data(data)
+                .pagination(pagination)
+                .build();
+    }
+
+}

6dc20423da29a68b338402e39c201f2ce4e4634b
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-06 14:30:37 +0900

    status

---
+++ springboot-admin/src/main/java/com/example/springbootadmin/model/enumClass/UserStatus.java
@@ -0,0 +1,16 @@
+package com.admin.springbootadmin.model.enumClass;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserStatus {
+
+    REGISTERED(0,"등록","사용자 등록상태"),
+    UNREGISTERED(1,"해지","사용자 해지상태");
+
+    private Integer id;
+    private String title;
+    private String description;
+}
